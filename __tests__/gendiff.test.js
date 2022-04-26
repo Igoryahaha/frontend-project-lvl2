d89785f15476d46034b60be029b847229c2573f6
Igor Kazantsev <i.kazancev@3commas.io>
2022-04-26 10:35:17 +0400

    fix env

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,3 +1,4 @@
+import { test, expect } from '@jest/globals';
 import fs from 'fs';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
@@ -8,7 +9,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-const readFixture = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8').trim();
+const readFixture = (filename) => fs.readFileSync(`${getFixturePath(filename)}`, 'utf-8').trim();
 
 const JSONOutputResult = readFixture('JSONOutput.txt');
 
